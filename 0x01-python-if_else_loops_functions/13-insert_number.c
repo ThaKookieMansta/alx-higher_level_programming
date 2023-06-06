944db18bd08a6e0673d7f81f5195db6de648b0f2
Chris Mukirae <chriskagiko@gmail.com>
2023-06-06 16:23:20 +0300

    Task 13 correction
    This is the commit of the 13th task

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,38 +1,37 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
-listint_t *create_node(int n);
+listint_t *create_nodes(int n);
 
 /**
- * insert_node - inserts a number into a sorted singly linked list
- * @head: double pointer to the head of the linked list
- * @number: the new data
- * Return: the address of the new node, or NULL if it failed
-*/
-
+ * insert_node - inserts a node sorted in a linked list of ints
+ * @head: double pointer to head of LL, needed for modification in edge
+ * cases
+ * @number: data for new node
+ *
+ * Return: pointer to newly created node, NULL on failure
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *this_node = NULL, *new_node = NULL;
 
 	if (!head)
-	{
 		return (NULL);
-	}
 	else if (!(*head))
 	{
-		new_node = create_node(number);
+		new_node = create_nodes(number);
 		*head = new_node;
 		return (new_node);
 	}
-
 	this_node = *head;
 	while (this_node)
 	{
+		/* need to insert at head */
 		if (this_node->n >= number)
 		{
-			new_node = create_node(number);
+			new_node = create_nodes(number);
 			new_node->next = this_node;
 			*head = new_node;
 			return (new_node);
@@ -41,7 +40,7 @@ listint_t *insert_node(listint_t **head, int number)
 		{
 			if (!this_node->next || this_node->next->n >= number)
 			{
-				new_node = create_node(number);
+				new_node = create_nodes(number);
 				new_node->next = this_node->next;
 				this_node->next = new_node;
 				return (this_node->next);
@@ -49,22 +48,24 @@ listint_t *insert_node(listint_t **head, int number)
 		}
 		this_node = this_node->next;
 	}
-	return (NULL);
-
+	return (NULL); /* failed */
 }
 
-/**
- * create_node - Creates nodes for the linked lists
- * @n: The node data
- * Return:PTR to the new node
-*/
-listint_t *create_node(int n)
-{
-	listint_t *r = NULL;
 
-	r = malloc(sizeof(listint_t));
-	if (!r)
-		return (NULL)
-	r->next = NULL;
-	r->n = n;
+/**
+ * create_nodes - creates a new node for the LL
+ * @n: data to insert into new node
+ *
+ * Return: pointer to newly allocated node
+ */
+listint_t *create_nodes(int n)
+{
+	listint_t *ret = NULL;
+
+	ret = malloc(sizeof(listint_t));
+	if (!ret)
+		return (NULL);
+	ret->next = NULL;
+	ret->n = n;
+	return (ret);
 }
