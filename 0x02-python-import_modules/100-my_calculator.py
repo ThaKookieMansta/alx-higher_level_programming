241630f315ca40a485e5f312b5a1a99261eb0a86
Chris Mukirae <chriskagiko@gmail.com>
2023-06-08 16:12:45 +0300

    Work Completion
    This commit is after the work was completed

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    no_of_arguments = len(argv) - 1
+    if no_of_arguments != 3:
+        print(f"Usage: {argv[0]} <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] == "+":
+        answer = add(a,b)
+    elif argv[2] == "-":
+        answer = sub(a,b)
+    elif argv[2] == "*":
+        answer = mul(a,b)
+    elif argv[2] == "/":
+        answer = div(a,b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print(f"{a} {argv[2]} {b} = {answer}")
