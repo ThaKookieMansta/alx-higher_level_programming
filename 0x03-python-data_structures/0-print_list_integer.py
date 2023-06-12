ad53d219a34e1252ea0c69553856da765c79145e
Chris Mukirae <chriskagiko@gmail.com>
2023-06-12 15:51:20 +0300

    Corrections No 0.1
    This commit is for all the corrections I have made

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    """
-    This function prints all integers in a list
-    :param my_list:
-    :return:
-    """
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
