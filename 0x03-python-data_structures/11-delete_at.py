d96967f70cec4ea5d560b0ebd9bb14db0a96be0c
Chris Mukirae <chriskagiko@gmail.com>
2023-06-12 14:48:23 +0300

    Task 11 and 12
    This commit is after completing tasks 11 and 12

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    """
+    A function that deletes the item at a specific position in a list.
+    :param my_list:
+    :param idx:
+    :return:
+    """
+
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    del my_list[idx]
+    return my_list
