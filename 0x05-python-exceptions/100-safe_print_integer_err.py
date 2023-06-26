0e123241b1cec53985476ffd52daa1220cbe822e
Chris Mukirae <chriskagiko@gmail.com>
2023-06-26 18:02:39 +0300

    Correction 100 v0.1
    Correction of the file

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+import sys
 def safe_print_integer_err(value):
     """
     This function Prints an integer
@@ -11,5 +11,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except (TypeError, ValueError) as e:
-        print("Exception: " + str(e))
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
