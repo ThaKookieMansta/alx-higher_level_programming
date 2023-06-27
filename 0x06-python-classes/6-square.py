43bd41d8345aac36a452530f824b9958a614fd41
Chris Mukirae <chriskagiko@gmail.com>
2023-06-27 21:28:57 +0300

    Correction no 6 v.6
    Correction of the file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -60,7 +60,11 @@ class Square:
         :param value:
         :return:
         """
-        if type(value) != tuple and int(value[0]) > 0 and int(value[1]) > 0:
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int or value[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__position = value
