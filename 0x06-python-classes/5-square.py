c868c59933cdde0edd6d0fb1443f2f0fa98c66eb
Chris Mukirae <chriskagiko@gmail.com>
2023-06-27 19:57:26 +0300

    Correction no 5 v.6
    Correction of the file

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -55,7 +55,11 @@ class Square:
         A print function
         :return:
         """
-        h = self.__size - 1
-        while h >= 0:
-            print("#" * self.__size)
-            h -= 1
+        if self.__size == 0:
+            print()
+        else:
+
+            h = self.__size
+            while h > 0:
+                print("#" * self.__size)
+                h -= 1
