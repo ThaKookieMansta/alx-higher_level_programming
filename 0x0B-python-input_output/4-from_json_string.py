4ddf1dbbf52badc26729be599efc50e81ab83d3b
Chris Mukirae <chriskagiko@gmail.com>
2023-07-11 22:10:23 +0300

    Corrections
    This commit contains the corrections for a number of files

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-This module contains the function def from_json_string(my_str)
+This module contains a function that returns an object represented
+by a JSON string
 """
 
 import json
