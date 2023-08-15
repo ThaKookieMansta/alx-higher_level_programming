04f3820144e190a09ce94df34552d7a6d4b084ed
Chris Mukirae <chriskagiko@gmail.com>
2023-08-15 19:53:46 +0300

    Corrections
    Correction on 2 files

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- script to delete the new db
-DROP DATABASE `hbtn_0c_0`;
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- This script adds a new table to the db
 -- It then adds rows of data
-CREATE TABLE `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1,'John', 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2,'Alex', 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3,'Bob', 14);
