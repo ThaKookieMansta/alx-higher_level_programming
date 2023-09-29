f48b51457c5c9ff1ba66e06a15b3d24d03f864d0
Chris Mukirae <chriskagiko@gmail.com>
2023-09-29 13:27:25 +0300

    Corrections on file 100
    Incorrect response

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,14 +7,15 @@ import requests
 
 
 if __name__ == "__main__":
-    github_api = f"https://api.github.com/repos/\
-    {sys.argv[2]}/{sys.argv[1]}/commits"
+    github_api = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
     my_response = requests.get(github_api)
     commits = my_response.json()
     try:
         for i in range(10):
-            print(f'{commits[i].get("sha")}: \
-            {commits[i].get("commit").get("author").get("name")}')
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
